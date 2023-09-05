f461550ebd8347f3fdfb7b6dc57fbb1aee436ca5
Alexis <alexis.draud2023@campus-eni.fr>
2023-09-05 15:06:49 +0200

    mise en place du fonctionnement de commande avec une liste

--- src/main/java/fr/eni/pizzaOnline/bo/Commande.java
+++ src/main/java/fr/eni/pizzaOnline/bo/Commande.java
@@ -1,7 +1,8 @@
 package fr.eni.pizzaOnline.bo;
 
 import java.io.Serializable;
-import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -16,9 +17,40 @@ public class Commande implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	private List<Produit> listProd = new ArrayList<>();
+
+	public Commande() {
+		super();
+	}
+
+	public Commande(Long id, List<Produit> listProd) {
+		super();
+		this.id = id;
+		this.listProd = listProd;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public List<Produit> getListProd() {
+		return listProd;
+	}
+
+	public void setListProd(List<Produit> listProd) {
+		this.listProd = listProd;
+	}
+	
+	
 //	private LocalDateTime dateHeureLivraison;
 //	private LocalDateTime dateHeurePreparation;
 
 //	private Etat etat;
+	
+	
 	
 }
--- src/main/java/fr/eni/pizzaOnline/controller/CommandeController.java
+++ src/main/java/fr/eni/pizzaOnline/controller/CommandeController.java
@@ -1,10 +1,33 @@
 package fr.eni.pizzaOnline.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import fr.eni.pizzaOnline.bo.Commande;
+import fr.eni.pizzaOnline.service.ProduitService;
 
 @Controller
 public class CommandeController {
 
+	public Commande commande = new Commande();
 	
+	@Autowired
+	ProduitService ps;
+	
+	@GetMapping("/commande/ajouter/{id:[0-9]+}")
+	public String ajouterAuPanier(@PathVariable long id) {
+		commande.getListProd().add(ps.ProduitParID(id));
+		System.out.println(commande.getListProd());
+		return "redirect:/carte";
+	}
+	
+	@GetMapping("/panier")
+	public String afficherPanier(Model model) {
+		model.addAttribute("produitsPanier",commande.getListProd());
+		return "commande/panier";
+	}
 	
 }
